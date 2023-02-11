b2448e320d66af2100fdfdf11d27660d49a4c06a
mjh9135 <1294845012@qq.com>
2023-02-12 00:46:31 +0800

    modify

--- job1.py
+++ job1.py
@@ -56,7 +56,7 @@ def main():
         career_recommendation = generate_text(prompt)
         st.success(career_recommendation)
 
-    st.write("这个工具是使用OpenAI 进行推荐任务，测试阶段，还有不足，有建议可以发送到邮件：rnmtq9@qq.com")
+    st.write("这个工具是使用OpenAI 进行推荐任务，测试阶段，还有不足，有建议可以发送到邮件：rnmtq@qq.com")
 
 if __name__ == "__main__":
     main()
